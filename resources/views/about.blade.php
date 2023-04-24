c65d6384b97d9c09866aa2f635bb058a6c28b1d7
Lalit Nayyar <lalitnayyar@gmail.com>
2023-04-25 03:02:09 +0400

    first push

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.base')
+@section('name', 'Lalit')
+
+
+@section('content')
+    <h1 class="text-4xl font-bold">About</h1>
+  @endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<x-base>
+    @section('name', 'Lalit')
+    <x-slot name="name">
+        Lalit
+    </x-slot>
+
+    <x-slot name="content">
+        <h1 class="text-4xl font-bold">Home</h1>
+        <p class="mt-4 text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque in saepe ea eius ex
+            recusandae
+            impedit earum! Aperiam quibusdam quos voluptate quisquam cum saepe incidunt odio delectus! Eius, obcaecati
+            asperiores.</p>
+    </x-slot>
+</x-base>
