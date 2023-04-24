d6873bdb6d4aa6e1437e77c2b719ad1ed23eda4b
Lalit Nayyar <lalitnayyar@gmail.com>
2023-04-25 03:38:20 +0400

    Chapter

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
     public function index() {
-        return view('welcome');
+        // 1. using raw Sql queries
+        // $users = DB::select('select * from users');
+        // dd($users);
+        // 2. Query builder
+        $users=DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
+        dd($users);
+        // 3. Elouent ORM
     }
 }
