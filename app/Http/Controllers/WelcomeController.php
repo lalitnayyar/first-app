4e9ed7d0297a66fe55d538d48d3fc38977512205
Lalit Nayyar <lalitnayyar@gmail.com>
2023-04-25 04:08:57 +0400

    2.08

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,18 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         // 1. using raw Sql queries
         // $users = DB::select('select * from users');
         // dd($users);
-        // 2. Query builder
-        $users=DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
-        dd($users);
+        // // 2. Query builder
+        // $users=DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
+        // dd($users);
         // 3. Elouent ORM
+        //$students = Student::all();
+        // $students = Student::select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+        // dd($students);
+        // foreach ($students as $student) {
+        //     echo $student->name . "<br>";
+        // }
+        $student = new Student();
+        $student->name = "New";
+        $student->email = "email@email.com";
+        $student->save();
     }
 }
